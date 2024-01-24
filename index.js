b82c06bcda2dc91bea6f9acdfbd4881814d7bd6c
Luke Garnsey <lukegarnsey@gmail.com>
2024-01-24 18:18:33 -0500

    Test framework now in devDepen, Added text.js class and tests for it, Added circle and square.js, added tests for shapes.js class, shapes class now distributes available shapes.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const inquire = require('inquirer');
 const Triangle = require('./lib/shapes');
 const fs = require('fs/promises');
+const Shape = require('./lib/shapes');
 
-const tri = new Triangle();
-console.log(tri.render());
-
-fs.writeFile('shape.html', tri.render()).then(()=>console.log("Complete")).catch(err=>console.log(err));
+// const tri = new Triangle();
+// console.log(tri.render());
+const shape = new Shape("square");
+console.log(shape.render());
+fs.writeFile('shape.html', shape.render())
+.then(()=>console.log("Complete"))
+.catch(err=>console.log(err));
