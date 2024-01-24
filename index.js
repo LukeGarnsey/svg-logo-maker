d4736ce9f41acf10e6f90f771868374a965bd2ca
Luke Garnsey <lukegarnsey@gmail.com>
2024-01-24 18:29:03 -0500

    Circle, Square, and Triangle all render correct SVG files

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const Shape = require('./lib/shapes');
 
 // const tri = new Triangle();
 // console.log(tri.render());
-const shape = new Shape("square");
+const shape = new Shape("circle");
 console.log(shape.render());
-fs.writeFile('shape.html', shape.render())
+fs.writeFile('shape.svg', shape.render())
 .then(()=>console.log("Complete"))
 .catch(err=>console.log(err));
