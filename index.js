87f4e80d7320f77c654e94342e5a91fffba4b797
Luke Garnsey <lukegarnsey@gmail.com>
2024-01-24 17:13:36 -0500

    Built out Triangle SVG, started work on test.js class

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const inquire = require('inquirer');
+const Triangle = require('./lib/shapes');
+const fs = require('fs/promises');
+
+const tri = new Triangle();
+console.log(tri.render());
+
+fs.writeFile('shape.html', tri.render()).then(()=>console.log("Complete")).catch(err=>console.log(err));
