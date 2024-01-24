d78dd89e013a50f7fae24ae8e2b04c23f1cd9e2d
Luke Garnsey <lukegarnsey@gmail.com>
2024-01-24 18:52:59 -0500

    Added test for all shapes, Shapes.js functionality changed for how it builds specific shape types.

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const inquire = require('inquirer');
-const Triangle = require('./lib/shapes');
 const fs = require('fs/promises');
 const Shape = require('./lib/shapes');
 
 // const tri = new Triangle();
 // console.log(tri.render());
-const shape = new Shape("circle");
+const shape = new Shape("triangle", "red");
 console.log(shape.render());
 fs.writeFile('shape.svg', shape.render())
 .then(()=>console.log("Complete"))
