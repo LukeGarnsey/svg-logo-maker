b781d8f3da0409d8aeae3d5ccc4fc00bd8aaaf15
Luke Garnsey <lukegarnsey@gmail.com>
2024-01-24 19:35:58 -0500

    Intial inquire prompt setup

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const inquire = require('inquirer');
 const fs = require('fs/promises');
-const Shape = require('./lib/shapes');
+const {AvailableShapes, Shape} = require('./lib/shapes');
 const Text = require('./lib/text');
 
-const text = new Text("LMG", "red", 100, 100 + 10, 60);
-const shape = new Shape("circle", "black", text);
 
-console.log(shape.render());
 
-fs.writeFile('shape.svg', shape.render())
-.then(()=>console.log("Complete"))
-.catch(err=>console.log(err));
+const questions = [
+  {
+    type:'list',
+    name:'shape',
+    message:'What shape would you like?',
+    choices: AvailableShapes
+  }
+];
+
+inquire.prompt(questions)
+.then((answers) =>{
+  const text = new Text("LMG", "red", 100, 100 + 10, 60);
+  const shape = new Shape(answers.shape, "black", text);
+
+  console.log(shape.render());
+
+  fs.writeFile('shape.svg', shape.render())
+  .then(()=>console.log("Complete"))
+  .catch(err=>console.log(err));
+});
