62e77c9f0bcd2594641cc7cf6f6a39f875e9d32d
Luke Garnsey <lukegarnsey@gmail.com>
2024-01-25 17:06:14 -0500

    Added example .svg files, prompt flow will loop if user enters too many characters. Simplified prompt flow. Added prompt for user to name their file. File writing moved to output folder, fixed tests with new changes.

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const fs = require('fs/promises');
 const Text = require('./lib/text');
 const textPrompt = require('./lib/prompts/text-prompt');
-const colorPrompt = require('./lib/prompts/color');
 const shapePrompt = require('./lib/prompts/shape-prompt');
+const inputPrompt = require('./lib/prompts/input-prompt');
 
 textPrompt().then(textObj => {
   shapePrompt(textObj).then(shapeObj =>{
-    fs.writeFile('shape.svg', shapeObj.render())
-    .then(()=>console.log("Complete"))
-    .catch(err=>console.log(err));
+    inputPrompt("What should I name the SVG file?").then(fileName =>{
+      fs.writeFile(`./output/${fileName}.svg`, shapeObj.render())
+      .then(()=>console.log("Complete"))
+      .catch(err=>console.log(err));
+    });
   });
 })
