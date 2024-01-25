69bc18ffbbaae3a8a6f242062416b3cb1fba2d29
Luke Garnsey <lukegarnsey@gmail.com>
2024-01-24 20:50:53 -0500

    Added prompt classes to work with prompt and return objects, built out prompt questions and prompt flow

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
-const inquire = require('inquirer');
 const fs = require('fs/promises');
-const {AvailableShapes, Shape} = require('./lib/shapes');
 const Text = require('./lib/text');
+const textPrompt = require('./lib/prompts/text-prompt');
+const colorPrompt = require('./lib/prompts/color');
+const shapePrompt = require('./lib/prompts/shape-prompt');
 
-
-
-const questions = [
-  {
-    type:'list',
-    name:'shape',
-    message:'What shape would you like?',
-    choices: AvailableShapes
-  }
-];
-
-inquire.prompt(questions)
-.then((answers) =>{
-  const text = new Text("LMG", "red", 100, 100 + 10, 60);
-  const shape = new Shape(answers.shape, "black", text);
-
-  console.log(shape.render());
-
-  fs.writeFile('shape.svg', shape.render())
-  .then(()=>console.log("Complete"))
-  .catch(err=>console.log(err));
-});
+textPrompt().then(textObj => {
+  shapePrompt(textObj).then(shapeObj =>{
+    fs.writeFile('shape.svg', shapeObj.render())
+    .then(()=>console.log("Complete"))
+    .catch(err=>console.log(err));
+  });
+})
