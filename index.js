b5e099fd0de3189394d0986f278cf291185a9616
Luke Garnsey <lukegarnsey@gmail.com>
2024-01-24 19:22:50 -0500

    Added Text to render pipeline, changed how render functions in shapes.js, changed each render function of Triangle, Square, Circle. Added testsfor shapes.js and text.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const inquire = require('inquirer');
 const fs = require('fs/promises');
 const Shape = require('./lib/shapes');
+const Text = require('./lib/text');
 
-// const tri = new Triangle();
-// console.log(tri.render());
-const shape = new Shape("triangle", "red");
+const text = new Text("LMG", "red", 100, 100 + 10, 60);
+const shape = new Shape("circle", "black", text);
+
 console.log(shape.render());
+
 fs.writeFile('shape.svg', shape.render())
 .then(()=>console.log("Complete"))
 .catch(err=>console.log(err));
